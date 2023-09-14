8642ab91c1a0792af4eefbd5c47c89aadb0be596
Lucas Niewohner <lucasniewohner@gmail.com>
2023-09-14 13:22:00 +1200

    Trying to work out why Mr. ant never wants to finish generating a route Also reshuffling the notebooks a bit

--- aco_algo/ACOSettings.py
+++ aco_algo/ACOSettings.py
@@ -9,7 +9,9 @@ class ACOSettings():
     traveled_discount: float # The factor by which traveled edges are less desireable
     deadendness_coeff: float # The factor by which deadends are more desireable
     directional_coeff: float # The factor by which go towards/away from the goal as needed, are more desireable
-    finish_boost: float # Multiplier for people from Finland (actually how likely the ants are to stop at the goal)
+    directional_choosiness: float # Coeff on directional sigmoid... higher = more difference between best and worst direction options
+    finish_boost: float # Multiplier for people from Finland (actually how likely the ants are to go to and stop at the goal)
+    gohome_boost: float # If the ant is over-distance, coeff for a quadratic boost to go home
 
     start_node: int
     goal_nodes: list[int]
--- aco_algo/Ant.py
+++ aco_algo/Ant.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from .ACOSettings import ACOSettings
 import numpy as np
+import itertools
 
 class Ant:
     def __init__(self, network_graph: nx.MultiGraph, settings: ACOSettings):
@@ -22,18 +23,29 @@ class Ant:
         self.route = [current_node]
         self.traveled_edges = set()
         self.traveled_length = 0
-        while current_node not in self.settings.goal_nodes:
-            outgoing_edges = self.network_graph.edges(current_node, data=True)
-            desireabilities = self.desireabilities(outgoing_edges)
-            desireabilities = desireabilities / np.sum(desireabilities)
-            chosen_edge = np.random.choice(outgoing_edges, p=desireabilities)
+        while current_node not in self.finish_nodes:
+            outgoing_edges = list(self.network_graph.edges(current_node, data=True))
+
+            if(len(self.route) <= 2):
+                # If we're just starting out, don't allow our poor ant to immediately finish
+                outgoing_edges = list(filter(lambda edge: edge[1] not in self.finish_nodes, outgoing_edges))
+
+            if len(outgoing_edges) == 1:
+                # If we go down a dead end, the only way out is back the way we came!
+                # We can skip the desireability calculation in this case.
+                chosen_edge = outgoing_edges[0]
+            else:
+                desireabilities = self.desireabilities(outgoing_edges)
+                desireabilities = desireabilities / np.sum(desireabilities)
+                chosen_ind = np.random.choice(len(outgoing_edges), 1, p=desireabilities)
+                chosen_edge = outgoing_edges[chosen_ind[0]]
 
             current_node = chosen_edge[1]
             self.route.append(current_node)
             self.traveled_edges.add(self.edge_sort(chosen_edge))
             self.traveled_length += chosen_edge[2]['length']
 
-        return self.route
+        return self.route, self.traveled_length
 
     def calc_dist_diff(self, edge):
         """
@@ -59,28 +71,37 @@ class Ant:
 
         # Add a boost for going towards/away from the goal
         # The correct direction is based purely on traveled distance and distance remaining to goal.
-        if self.settings.directional_coeff > 0:
+        gohome = self.settings.gohome_boost > 0 and self.traveled_length > self.settings.target_length
+        if self.settings.directional_coeff > 0 or gohome:
             # Calculate dist diffs (see function comment)
             dist_diffs = np.array([self.calc_dist_diff(edge) for edge in edges])
-            # Normalize diffs for relative comparison
-            dist_diffs = (dist_diffs - np.mean(dist_diffs)) / np.std(dist_diffs)
-            # Smack it with a sigmoid (https://eelslap.com/)
-            dist_diffs = 1 / (1 + np.exp(-dist_diffs))
-            # Finally, scale and add to heuristics
-            heuristics += dist_diffs * self.settings.directional_coeff
+            # if gohome:
+            #     # Add a quadratically-increasing boost for going home
+            #     # As distance increases beyond the target, this should quickly cause the ant to follow the shortest path to a goal.
+            #     heuristics += (dist_diffs-np.max(dist_diffs))**2 * self.settings.gohome_boost
+            if self.settings.directional_coeff > 0:
+                # Normalize diffs for relative comparison
+                dist_diffs_norm = (dist_diffs - np.mean(dist_diffs)) / np.std(dist_diffs)
+                # Smack it with a sigmoid (https://eelslap.com/)
+                # Leave out the negative sign because we want this to be a decreasing function (more diff = less desireable)
+                dist_diffs_sig = 1 / (1 + np.exp(dist_diffs_norm * self.settings.directional_choosiness))
+                # Finally, scale and add to heuristics
+                heuristics += dist_diffs_sig * self.settings.directional_coeff
 
-        # Add a boost for deadend-ier nodes
-        heuristics += np.array([end_node['deadendness'] for end_node in end_nodes]) * self.settings.deadendness_coeff
-        # Remove a penalty for traveled edges
-        heuristics += 1 - np.array([edge[2]['traveled'] or self.edge_sort(edge) in self.traveled_edges for edge in edges]) * self.settings.traveled_discount
+        # The lads that have traveled
+        traveled_lads = np.array([edge[2]['traveled'] or self.edge_sort(edge) in self.traveled_edges for edge in edges])
+        # Add a boost for deadend-ier nodes (except for traveled deadends)
+        heuristics += np.array([end_node['deadendness'] for end_node in end_nodes]) * self.settings.deadendness_coeff * traveled_lads
         # Add a constant boost factor for finish nodes
         heuristics += np.array([end_node['is_finish_node'] for end_node in end_nodes]) * self.settings.finish_boost
-        # TODO: Consider precomputing constant-to-end-node factors (deadendness, is_finish_node, etc) whenever settings are updated
+        # TODO: Consider precomputing these factors that are constant to given node, traveled status, and settings 
+        # (deadendness, is_finish_node, etc) 
         
         # Add other heuristics here
 
         pheromones = np.array([edge[2]['pheromone'] for edge in edges])
 
         desireabilities = pheromones**self.settings.pheromone_weight * \
-            heuristics**self.settings.heuristic_weight
+            heuristics**self.settings.heuristic_weight * \
+            (1 - traveled_lads * self.settings.traveled_discount)
         return desireabilities
